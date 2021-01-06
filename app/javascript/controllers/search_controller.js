f337cde76722878c1e8c33c736b1b2865a611512
angarc <angarc37@gmail.com>
2021-01-06 00:32:09 -0700

    Styled the search bar

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,24 +1,31 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "input", "container" ]
+  static targets = [ "input", "container", "searchIcon" ]
 
   connect() {
   }
 
   getSuggestions() {
-    let container = this.containerTarget
-    const queryLength = this.inputTarget.value.length
-    console.log(queryLength);
+    let container   = this.containerTarget
+    let queryLength = this.inputTarget.value.length
+    let input       = this.inputTarget
+    let searchIcon  = this.searchIconTarget
 
     if (queryLength == 0) {
       console.log("should hide");
       if (!container.classList.contains("hidden")) {
-
-        console.log("hide!");
-        console.log(queryLength);
         container.classList.add("hidden") 
       }
+
+      if (input.classList.contains("suggestions-active")) {
+        input.classList.remove("suggestions-active");
+      }
+
+      if (searchIcon.classList.contains("suggestions-active")) {
+        searchIcon.classList.remove("suggestions-active");
+      }
+
     } else {
       fetch(`/get_suggestions/?q=${this.inputTarget.value}`)
         .then(response => {
@@ -27,6 +34,14 @@ export default class extends Controller {
         .then(data => {
           container.innerHTML = data
           container.classList.remove("hidden")
+
+          if (!input.classList.contains("suggestions-active")) {
+            input.classList.add("suggestions-active");
+          }
+
+          if (!searchIcon.classList.contains("suggestions-active")) {
+            searchIcon.classList.add("suggestions-active");
+          }
         })
     }
   }
